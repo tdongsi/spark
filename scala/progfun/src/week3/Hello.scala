41f748342d53dc35e7889807c3287b866e46aacf
tdongsi <dongsi.tuecuong@gmail.com>
2016-10-01 21:34:51 -0700

    Coursera: HelloWorld with object definition.

---
+++ scala/progfun/src/week3/Hello.scala
@@ -0,0 +1,10 @@
+package week3
+
+/**
+  * Created by epigineer on 10/1/2016.
+  */
+object Hello {
+  def main(args: Array[String]) = {
+    println("Hello World 2!")
+  }
+}
---
+++ scala/progfun/src/week3/intsets.sc
@@ -0,0 +1,8 @@
+object intsets {
+  println("Welcome")
+}
+
+abstract class IntSet {
+  def incl(x: Int): IntSet
+  def contains(x: Int): Boolean
+}
