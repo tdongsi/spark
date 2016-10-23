b57d89865f07909216e06df3386676cb7cfbb3d9
tdongsi <dongsi.tuecuong@gmail.com>
2016-10-22 23:50:13 -0700

    Coursera: Assignment 5: Computing Subsets of a Set.

--- scala/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ scala/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -93,7 +93,10 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+    val occurs = occurrences.map( x => (for (i <- 1 to x._2) yield (x._1, i)).toList )
+    occurs.foldRight(List[Occurrences](Nil))( (x, y) => y ++ (for (i <-x; j <-y) yield i :: j))
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
